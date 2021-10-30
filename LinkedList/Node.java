8c7c89123c68225674416fef50e0d1cfa146ab15
Hernan Cedillo <kermit_cedillo@yahoo.com>
2021-10-30 16:33:10 -0700

    show how the data structures are connected with one anther

---
+++ LinkedList/Node.java
@@ -0,0 +1,27 @@
+/**
+ * Linked list is a data structure that store a collection of items 
+ * drawbacks for arrays, if you intert items more than the size 
+ * you have to recreate a new array and recopy all of the elements with 
+ * a different size array, which means that it is memory intensive
+ * you can insert items in the beginning of the list and back of the list 
+ * also in the middle.
+ * 
+ * You can grow the size of the collection
+ * you can change the links to point at other nodes
+ * like an array you cannot change the address in wihch the 
+ * data is being stored
+ * 
+ * if you delete something in the middle 
+ * the nodes will point at the next element 
+ */
+
+public class Node{
+    //recursive data type, 
+    Node next = null; 
+    int data; 
+
+    
+
+
+
+}
