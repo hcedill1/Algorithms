646e91a10afd9846e32656b51e4d2374266be379
Hernan Cedillo <kermit_cedillo@yahoo.com>
2021-10-29 21:47:01 -0700

    Adding private variables to build a stack

--- Stacks/Stacks.java
+++ Stacks/Stacks.java
@@ -1,4 +1,13 @@
 public class Stacks{ 
 
+    private int maxSize; 
+    private long[] stackArray; 
+    private int top; 
+
+    public Stacks(int size){
+        this.maxSize = size;
+        this.stackArray = new long[maxSize];
+        this.top = -1;
+    }
 
 }
